bcfeaffe0e6ff8878757bfedb1437f144d1efe21
SeoJunOH <lupets13@gmail.com>
2022-09-30 10:36:10 +0900

    restaurant

--- controllers/lodgmentController.js
+++ controllers/lodgmentController.js
@@ -1,26 +1,142 @@
 const lodgmentService = require('../services/lodgmentService')
+const lodgmentDao = require('../models/lodgmentDao')
+
+const lodgmentList = async (req, res) => {
+    console.log("START lodgmentListController")
+    var {
+        숙소유형, hashtag, facility, 지역검색, startDate, endDate, 
+        adultCnt, childCnt, 호텔등급, 별점, sort, offset, 
+        limit} = req.query
+
+        address = 지역검색
+        tag = 숙소유형
+        reviewPoint = 별점
+        starPoint = 호텔등급
+
+    
+    var lodgmentList = await lodgmentService.lodgmentList(tag, hashtag, facility, address, startDate, endDate, adultCnt, childCnt, starPoint, reviewPoint, sort, offset, limit)
+    var lengthCheck = await lodgmentService.lodgmentList(tag, hashtag, facility, address, startDate, endDate, adultCnt, childCnt, starPoint, reviewPoint, sort, 0, 10000)
+    var totalCount = lengthCheck.length
+    console.log('END lodgmentListController')
+    res.status(200).json({lodgmentList:lodgmentList, totalCount:totalCount})
+}
 
 const lodgment = async (req, res) => {
     console.log('START lodgmentController')
-    const {id} = req.params
+    const { id } = req.params
     console.log(id)
-    const lodgment = await lodgmentService.lodgment(id)
-    const lodgmentRoom = await lodgmentService.lodgmentRoom(id)
+    let lodgment = await lodgmentService.lodgment(id)
+    let lodgmentRoom = await lodgmentService.lodgmentRoom(id)
     const lodgmentReview = await lodgmentService.lodgmentReview(id)
 
     console.log('END lodgmentController')
-    res.status(200).json({lodgment: lodgment , room: lodgmentRoom, review: lodgmentReview})
+    res.status(200).json({ lodgment: lodgment, room: lodgmentRoom, review: lodgmentReview })
 }
-const lodgmentReviewWrite = async(req,res) => {
+const lodgmentReviewWrite = async (req, res) => {
     console.log('START lodgmentReviewWriteController')
-    const {token, lodgmentId, review, cleanPoint, facilityPoint, servicePoint, costperformancePoint, photo} = req.body
+    const { token, lodgmentId, review, cleanPoint, facilityPoint, servicePoint, costperformancePoint, photo } = req.body
     const lodgmentReviewWrite = await lodgmentService.lodgmentReviewWrite(token, lodgmentId, review, cleanPoint, facilityPoint, servicePoint, costperformancePoint, photo)
-    
+
     console.log('END lodgmentReviewWriteController')
-    res.status(200).json({message: 'review Written'})
+    res.status(200).json({ message: 'review Written' })
+}
+
+const lodgmentReviewDelete = async (req,res) => {
+    const {token, reviewId, lodgmentId} = req.body
+    const lodgmentReviewDelete =  await lodgmentService.lodgmentReviewDelete(reviewId, token, lodgmentId)
+    res.status(200).json({ message: 'review Deleted'})
+}
+
+const lodgmentLike = async (req,res) => {
+    console.log('')
+    const {token, lodgmentId} = req.body
+    const lodgmentLike = await lodgmentService.lodgmentLike(token, lodgmentId)
+    console.log('')
+    res.status(200).json({ message: 'like written'})
+}
+
+const lodgmentBookMark = async (req,res) => {
+    console.log('')
+    const {token, lodgmentId} = req.body
+    const lodgmentBookMark = await lodgmentService.lodgmentBookMark(token, lodgmentId)
+    console.log('')
+    res.status(200).json({ message: 'bookMark written'})
+}
+
+const lodgmentTotalInsert = async(req,res) => {
+    console.log('lodgmentTotalInsert')
+    let {
+        name, intro, phoneNumber, useInfo, starPoint,
+        facility, hashtag, tag,
+        regionAddress, fullAddress, googleAddress,
+        photo
+    } = req.body
+
+    if(starPoint==''||starPoint==undefined){
+        starPoint = 0
+    }
+
+    const lodgmentTotalInsert = await lodgmentDao.lodgmentTotalInsert(        
+        name, intro, phoneNumber, useInfo, starPoint,
+        facility, hashtag, tag,
+        regionAddress, fullAddress, googleAddress,
+        photo)
+    
+    var lodgmentId = lodgmentTotalInsert[0].id
+    res.status(200).json({message:'created',lodgmentId:lodgmentId})
+}
+
+const lodgmentReview = async (req, res) => {
+    console.log('START lodgmentController')
+    const { id, category } = req.query
+    const lodgmentReview = await lodgmentService.lodgmentReview(id, category)
+    const lodgmentNameget = await lodgmentService.lodgment(id)
+    let lodgmentName = lodgmentNameget.name
+
+    let cleanAverage = 0
+    let facilityAverage = 0
+    let serviceAverage = 0
+    let costperformanceAverage = 0
+    let reviewAverage = 0
+    for(let i=0; i<lodgmentReview.length;i++){
+        cleanAverage = cleanAverage+lodgmentReview[i].cleanPoint
+        facilityAverage = facilityAverage+lodgmentReview[i].facilityPoint
+        serviceAverage = serviceAverage+lodgmentReview[i].servicePoint
+        costperformanceAverage = costperformanceAverage+lodgmentReview[i].costperformancePoint
+        reviewAverage = reviewAverage+lodgmentReview[i].reviewPoint
+    }
+    cleanAverage = cleanAverage/lodgmentReview.length
+    facilityAverage = facilityAverage/lodgmentReview.length
+    serviceAverage = serviceAverage/lodgmentReview.length
+    costperformanceAverage = costperformanceAverage/lodgmentReview.length
+    reviewAverage = reviewAverage/lodgmentReview.length
+
+    console.log('END lodgmentController')
+    res.status(200).json({totalAverage:reviewAverage,
+        cleanAverage:cleanAverage,
+        facilityAverage:facilityAverage,
+        serviceAverage:serviceAverage,
+        costperformanceAverage:costperformanceAverage,
+        lodgmentName:lodgmentName,
+        lodgmentReview:lodgmentReview
+        })
+}
+
+const lodgmentRoomInsert = async(req,res) => {
+    console.log('lodgmentRoomInsert')
+    const {lodgmentId, roomName, peopleNumber, smoking, bedInfo, price} = req.body
+    const lodgmentRoomInsert = await lodgmentDao.lodgmentRoomInsert(lodgmentId, roomName, peopleNumber, smoking, bedInfo, price)
+    res.status(200).json({message:'created'})
 }
 
 module.exports = {
+    lodgmentList,
     lodgment,
     lodgmentReviewWrite,
+    lodgmentReviewDelete,
+    lodgmentLike,
+    lodgmentBookMark,
+    lodgmentTotalInsert,
+    lodgmentReview,
+    lodgmentRoomInsert,
 };
--- services/lodgmentService.js
+++ services/lodgmentService.js
@@ -3,17 +3,74 @@ const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 dotenv.config()
 
+const lodgmentList = async (tag, hashtag, facility, address, startDate, endDate, adultCnt, childCnt, starPoint, reviewPoint, sort, offset, limit) => {
+    console.log('START lodgmentListService')
+    if (tag == undefined) {
+        tag = 'null'
+    }
+    if (hashtag == undefined) {
+        hashtag = 'null'
+    }
+    if (facility == undefined) {
+        facility = 'null'
+    }
+    if (address == undefined) {
+        address = 'null'
+    }
+    if (starPoint == undefined) {
+        starPoint = 'null'
+    }
+    if (reviewPoint == undefined) {
+        reviewPoint = 'null'
+    }
+    if (sort == undefined) {
+        sort = 'null'
+    }
+    if (startDate == undefined || endDate == undefined) {
+        startDate = '12345'
+        endDate = '12345'
+    } else {
+        startDate = '%' + startDate + '%'
+        endDate = '%' + endDate + '%'
+    }
+    if(offset==undefined||offset=='0'){
+        offset=0
+    }
+    if(limit==undefined){
+        limit=10000
+    }
+    if (adultCnt == undefined || childCnt == undefined) {
+        peopleNumber = 'null'
+    } else {
+        peopleNumber = adultCnt + childCnt
+    }
+
+    const lodgmentList = await lodgmentDao.lodgmentList(tag, hashtag, facility, address, startDate, endDate, peopleNumber, starPoint, reviewPoint, sort, offset, limit)
+
+    for (let i = 0; i < lodgmentList.length; i++) {
+        lodgmentList[i].tag = JSON.parse(lodgmentList[i].tag)
+        lodgmentList[i].hashTag = JSON.parse(lodgmentList[i].hashTag)
+        lodgmentList[i].facility = JSON.parse(lodgmentList[i].facility)
+        lodgmentList[i].photo = JSON.parse(lodgmentList[i].photo)
+        lodgmentList[i].peopleNumber = JSON.parse(lodgmentList[i].peopleNumber)
+    }
+    console.log('END lodgmentListService')
+    return lodgmentList
+}
+
 const lodgment = async (id) => {
     console.log('START lodgmentService')
     const lodgments = await lodgmentDao.lodgment(id)
     const lodgment = lodgments[0]
     lodgment.photo = JSON.parse(lodgment.photo)
     lodgment.facility = JSON.parse(lodgment.facility)
+    lodgment.hashTag = JSON.parse(lodgment.hashTag)
+    lodgment.tag = JSON.parse(lodgment.tag)
 
     const lodgmentLike = await lodgmentDao.lodgmentLike(id)
     lodgment.totalLike = lodgmentLike.length
 
-    console.log('END createUserService');
+    console.log('END lodgmentService');
     return lodgment
 }
 
@@ -29,14 +86,13 @@ const lodgmentRoom = async (id) => {
     return lodgmentRoom
 }
 
-const lodgmentReview = async (id) => {
+const lodgmentReview = async (id, category) => {
     console.log('START lodgmentReivewService')
-    const lodgmentReview = await lodgmentDao.lodgmentReview(id)
-    for (let i = 0; i < lodgmentReview.length; i++) {
-        if (lodgmentReview[i].photo != 'NULL') {
-            lodgmentReview[i].photo = JSON.parse(lodgmentReview[i].photo)
-        }
+    if(category==undefined){
+        category='null'
     }
+
+    const lodgmentReview = await lodgmentDao.lodgmentReview(id, category)
     console.log('END lodgmentReviewService')
     return lodgmentReview
 }
@@ -49,8 +105,8 @@ const lodgmentReviewWrite = async (token, lodgmentId, review, cleanPoint, facili
     const reviewPoint = (cleanPoint + facilityPoint + servicePoint + costperformancePoint) / 4
     if (photo != undefined) {
         category = 'photo'
-    }else{
-        category = null
+    } else {
+        category = 'null'
     }
     const lodgmentReviewWrite = await lodgmentDao.lodgmentReviewWrite(userId, lodgmentId, category, review, cleanPoint, facilityPoint, servicePoint, costperformancePoint, reviewPoint)
 
@@ -58,20 +114,73 @@ const lodgmentReviewWrite = async (token, lodgmentId, review, cleanPoint, facili
     if (photo != undefined) {
         for (let i = 0; i < reviewList.length; i++) {
             if (reviewList[i].userId == userId && reviewList[i].lodgmentId == lodgmentId && reviewList[i].review == review) {
-                for (let j = 0; j < photo.length; j++) {
-                    await lodgmentDao.lodgmentReviewPhotoWrite(reviewList[i].id, photo[j])
-                }
+                await lodgmentDao.lodgmentReviewPhotoWrite(reviewList[i].id, photo)
             }
         }
     }
+
+    const lodgmentReview = await lodgmentDao.lodgmentReview(lodgmentId,"null","null")
+    let averageReviewPoint = 0
+    for (let i = 0; i < lodgmentReview.length; i++) {
+        averageReviewPoint = averageReviewPoint + lodgmentReview[i].reviewPoint
+    }
+    let reviewCount = lodgmentReview.length
+    averageReviewPoint = averageReviewPoint / lodgmentReview.length
+    const lodgmentaverageReviewPoint = await lodgmentDao.lodgmentaverageReviewPoint(averageReviewPoint, reviewCount, lodgmentId)
+
 
     console.log('END lodgmentReviewWirteService')
     return lodgmentReviewWrite
 }
 
+const lodgmentReviewDelete = async (reviewId, token, lodgmentId) => {
+    const key = process.env.SECRET_KEY
+    const tokenId = jwt.verify(token, key)
+    const userId = tokenId.userId
+
+    const lodgmentReviewDelete = await lodgmentDao.lodgmentReviewDelete(reviewId, userId)
+
+    const lodgmentReview = await lodgmentDao.lodgmentReview(lodgmentId)
+    let averageReviewPoint = 0
+    for (let i = 0; i < lodgmentReview.length; i++) {
+        averageReviewPoint = averageReviewPoint + lodgmentReview[i].reviewPoint
+    }
+    let reviewCount = lodgmentReview.length
+    averageReviewPoint = averageReviewPoint / lodgmentReview.length
+    const lodgmentaverageReviewPoint = await lodgmentDao.lodgmentaverageReviewPoint(averageReviewPoint, reviewCount, lodgmentId)
+
+    return lodgmentReviewDelete
+}
+
+const lodgmentLike = async (token, lodgmentId) => {
+    const key = process.env.SECRET_KEY
+    const tokenId = jwt.verify(token, key)
+    const userId = tokenId.userId
+
+    const lodgmentLikeInsert = await lodgmentDao.lodgmentLikeInsert(userId, lodgmentId)
+    const lodgmentLike = await lodgmentDao.lodgmentLike(lodgmentId)
+    let totalLike = lodgmentLike.length
+    const lodgmentTotalLike = await lodgmentDao.lodgmentTotalLike(totalLike, lodgmentId)
+    return
+}
+
+const lodgmentBookMark = async (token, lodgmentId) => {
+    const key = process.env.SECRET_KEY
+    const tokenId = jwt.verify(token, key)
+    const userId = tokenId.userId
+
+    const lodgmentBookMark = await lodgmentDao.lodgmentLikeInsert(userId, lodgmentId)
+    return lodgmentBookMark
+}
+
+
 module.exports = {
+    lodgmentList,
     lodgment,
     lodgmentRoom,
     lodgmentReview,
+    lodgmentLike,
     lodgmentReviewWrite,
+    lodgmentReviewDelete,
+    lodgmentBookMark,
 };
